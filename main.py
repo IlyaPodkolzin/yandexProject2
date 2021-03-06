f4487c8210e56684266600d83a94096e6baafc12
IlyaPodkolzin <pdk.04@mail.ru>
2021-03-06 23:48:59 +0300

    /training

--- main.py
+++ main.py
@@ -9,5 +9,13 @@ def index(title):
     return render_template('base.html', title=title)
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    engineer_prof = False
+    if "инженер" in prof.lower() or "строитель" in prof.lower():
+        engineer_prof = True
+    return render_template('training.html', engineer_prof=engineer_prof)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
